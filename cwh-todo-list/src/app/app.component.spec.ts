0cde9ede7698cb5baec655cd3bd92eb8dda10dbb
AtturiAkanksha <akanksha.a@technovert.com>
2023-01-31 16:48:22 +0530

    commit

--- cwh-todo-list/src/app/app.component.spec.ts
+++ cwh-todo-list/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cwh-todo-list'`, () => {
+  it(`should have as title 'employee-directory-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('cwh-todo-list');
